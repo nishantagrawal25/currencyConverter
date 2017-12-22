cddcb0cbe2948236283db36c24ed6302ccbd79bd
Nishant Agrawal <nishant.agrawal@industrybuying.com>
2017-12-22 16:27:43 +0530

    added currencylist screen

---
+++ app/components/List/Icon.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, Image } from 'react-native';
+
+import styles from './styles';
+
+const Icon = ({ checkmark, visible }) => {
+  const iconStyles = [styles.icon];
+  if (visible) {
+    iconStyles.push(styles.iconVisible);
+  }
+
+  return (
+    <View style={iconStyles}>
+      {checkmark ? (
+        <Image
+          style={styles.checkIcon}
+          resizeMode="contain"
+          source={require('./images/check.png')}
+        />
+      ) : null}
+    </View>
+  );
+};
+
+Icon.proptypes = {
+  checkmark: PropTypes.bool,
+  visible: PropTypes.bool,
+};
+
+export default Icon;
--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 import Home from './screens/Home';
+import CurrencyList from './screens/CurrencyList';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
@@ -9,6 +10,7 @@ EStyleSheet.build({
   $border: '#e2e2e2',
   $inputText: '#797979',
   $lightGray: '#F0F0F0',
+  $darkText: '#343434',
 });
 
 export default () => <Home />;
